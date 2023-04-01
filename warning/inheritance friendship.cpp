56552726582c0e9be2f7624fe35600c21e41f365
Jiwon <morex1566@gmail.com>
2023-04-02 07:18:40 +0900

    [2023-04-02] inherit friendship
    '아들 : 부모의 친구라고 해서, 그 사람이 내 친구이진 않잖아'

---
+++ warning/inheritance friendship.cpp
@@ -0,0 +1,40 @@
+/*
+	warning	: 'friend' keyword는 상속받지 않습니다.
+	해결	: 각각 개별적으로 선언해줘야 합니다.
+	의미	: 'friend'는 상속받지 않습니다.
+			 1. 부모의 친구라고 해서, 자식의 친구라는 주장은 모호합니다.
+             2. 개별적으로 친구를 선언해줘야 합니다.
+*/
+
+#include <iostream>
+
+class ParentFrined;
+class Son;
+class Parent;
+
+class ParentFrined
+{
+    friend class Parent;
+private:
+    int data = 4;
+};
+
+class Parent
+{
+public:
+    ParentFrined pf = ParentFrined();
+
+    void function()
+    {
+        pf.data;
+    }
+};
+
+class Son : public Parent
+{
+    void function()
+    {
+        // warning triggered!
+        pf.data;
+    }
+};
