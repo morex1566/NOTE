6bd51523f398d69a175f586f40478c7be4d82ca6
범지원 <morex1566@gmail.com>
2023-06-25 18:46:43 +0900

    [2023-06-25] 언리얼 5.2 테스트 프로젝트 초기화

---
+++ Unreal/TEST/Source/TEST/TEST.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TEST.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TEST, "TEST" );
+ 
