f8603dd08ea8b3cc009801a099aee1f885c453fd
범지원 <morex1566@gmail.com>
2023-08-15 20:05:55 +0900

    [2023-08-15] assimp sdk 적용 및 프로젝트 설정

--- Console/Engine/Core/pch.h
+++ Console/Engine/Core/pch.h
@@ -1,7 +1,20 @@
 #pragma once
 
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
 #include <vector>
 #include <memory>
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
 
+#include <wrl/client.h>
+#include <DirectXMath.h>
+#include <d3d11.h>
 
 using uint = unsigned int;
+
+using namespace Microsoft::WRL;
+using namespace DirectX;
+using namespace std;
