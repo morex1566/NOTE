d46298b46c380291584c332ea40222eb002a8db3
Jiwon <morex1566@gmail.com>
2023-10-31 20:20:44 +0900

    [2023-10-31] Change test solution name

--- Test/Sources/Core/main.cpp
+++ Test/Sources/Core/main.cpp
@@ -4,13 +4,14 @@
 #include <string>
 #include <memory>
 
+class GameInstance
+{
+private:
+    uint8_t bIsEnabled : 1;
+};
+
 int main() {
 
-	std::unique_ptr<int> datauptr = std::make_unique<int>(1);
-
-	int* dataptr = datauptr.get();
-
-	*dataptr = 2;
 
     return 0;
 }
