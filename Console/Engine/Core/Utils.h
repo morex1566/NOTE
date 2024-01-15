3d03aae70bd32037531bddfc712e78b757b1be2c
Jiwon <morex1566@gmail.com>
2024-01-15 18:08:09 +0900

    [2024-01-15] Delete : Useless files.

--- Console/Engine/Core/Utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-inline std::wstring StringToWString(const std::string& narrowString) {
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    return converter.from_bytes(narrowString);
-}
